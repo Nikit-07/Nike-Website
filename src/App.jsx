e77fd9d9f3b9cd5a6eb96ac59550ad8b0d5202f3
Nikit <nikitbhakuni094@gmail.com>
2024-11-06 20:19:55 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <main className='relative'>
+      Nav
+
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+       <Hero/>
+      </section>
+
+      <section className='padding'>
+        Popular Product
+      </section>
+      <section className='padding'>
+        Super Quality
+      </section>
+      <section className='padding-x py-10'>
+        Services
+      </section>
+      <section className='padding'>
+        Special Offers
+      </section>
+      <section className=' bg-pale-blue padding'>
+        Customer Reviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        Subscribe
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        Footer
+      </section>
+    </main>
+
+  )
+}
+
+export default App
